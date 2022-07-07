c412c169c707f31aa26b8d4e1d8389ac609ea7d6
shubham-mrk <108858431+shubham-mrk@users.noreply.github.com>
2022-07-07 12:17:06 +0530

    Add my 7th july files
    Add my 7th july files

---
+++ 7july.js
@@ -0,0 +1,15 @@
+function Add(){
+	console.log('hello html');
+	var v= document.getElementById('t1').value;
+	var result =2*+v
+	console.log(result);
+}
+
+function Clear(){
+	var c = document.getElementById('t1').value
+	document.getElementById('result').textContent="result is "+2*c ;
+}
+
+function sort(){
+	document 
+}
