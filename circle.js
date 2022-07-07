8e9280cb1090163d38e3a899e6faf3998e6c7cfa
shubham-mrk <108858431+shubham-mrk@users.noreply.github.com>
2022-07-07 13:45:27 +0530

    Add files via upload

---
+++ circle.js
@@ -0,0 +1,11 @@
+function area(){
+var v1= document.getElementById('v1').value;
+var result=(3.14*+v1*+v1);
+document.getElementById('v2').value = result;
+}
+
+function perimeter(){
+var v1= document.getElementById('v1').value;
+var result=(2*3.14*+v1);
+document.getElementById('v3').value = result;
+}
